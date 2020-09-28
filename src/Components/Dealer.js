b9dde6d468a05ae7819f675d1e3597d02d7b94f6
micahmosley <micah.mosley@utexas.edu>
2020-09-28 11:59:13 -0500

    added game component

---
+++ src/Components/Dealer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dealer extends Component {
+  render() {
+    return (
+      <div >
+        
+      </div>
+    );
+  }
+}
+
+export default Dealer;
--- src/Containers/Game.js
+++ src/Containers/Game.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
+import Dealer from "../Components/Dealer";
 
 class Game extends Component {
+  
+    state={
+        dealerCards=[],
+        userCards=[],
+        deck=[],
+        funds=0,
+    }
+
+
+
   render() {
     return (
       <div >
