b2169e60418c3c9a9122934606c2d75761a28e71
cam4994 <colin.a.mosley@gmail.com>
2020-10-02 11:43:57 -0500

    Made funds show up even after game over. Styled modal and home link

--- src/Components/Funds.js
+++ src/Components/Funds.js
@@ -4,27 +4,32 @@ class Funds extends Component {
 
     render() {
         return (
-            
+
             <div className="inner-funds-container">
-                {this.props.gameOver===false ? <div className="funds"> <div className="remainingFunds">
-                    <h2>Available Funds: <span className='bold' style={{color: 'green'}}>${this.props.funds}</span></h2>
+                <div className="funds">
+                    <div className="remainingFunds">
+                        <h2>Available Funds: <span className='bold' style={{ color: 'green' }}>${this.props.funds}</span></h2>
+                    </div>
+                    {this.props.gameOver === false ?
+                        <div>
+                            <div className="betTotal">
+                                <h3>Bet Amount: <span className='bold' style={{ color: 'green' }}>${this.props.bet}</span></h3>
+                            </div>
+                            <div className="fancybuttons">
+                                <a href="#" onClick={() => this.props.addToBet(5)} className="red" data-speed="4" data-color="#f33">$5</a>
+                                <a href="#" onClick={() => this.props.addToBet(10)} className="green" data-speed="4" data-color="#3f3">$10</a>
+                                <a href="#" onClick={() => this.props.addToBet(25)} className="pink" data-speed="4" data-color="#39f">$25</a>
+                            </div>
+
+                            <div className="otherButtons">
+                                <a href="#" onClick={this.props.clearBet} className="blue" data-speed="4" data-color="#39f">Clear</a>
+                                <a href="#" onClick={this.props.startGame} className="blue" data-speed="4" data-color="#39f">Bet</a>
+                            </div>
+                        </div> : null}
                 </div>
-                <div className="betTotal">
-                    <h3>Bet Amount: <span className='bold' style={{color: 'green'}}>${this.props.bet}</span></h3>
-                </div>
-                <div className="fancybuttons">
-                    <a href="#" onClick={() => this.props.addToBet(5)} className="red" data-speed="4" data-color="#f33">$5</a>
-                    <a href="#" onClick={() => this.props.addToBet(10)} className="green" data-speed="4" data-color="#3f3">$10</a>
-                    <a href="#" onClick={() => this.props.addToBet(25)} className="pink" data-speed="4" data-color="#39f">$25</a>
-                </div>
-                
-                <div className="otherButtons">
-                    <a href="#" onClick={this.props.clearBet} className="blue" data-speed="4" data-color="#39f">Clear</a>
-                    <a href="#" onClick={this.props.startGame} className="blue" data-speed="4" data-color="#39f">Bet</a>
-                </div></div> : null}
-                </div>
-                
-           
+            </div>
+
+
         );
     }
 }
