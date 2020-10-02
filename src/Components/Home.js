f0863a847633454ac5de7f664c2ef04f7b3a069a
micahmosley <micah.mosley@utexas.edu>
2020-10-02 10:49:27 -0500

    changed home page design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,6 @@ import React from 'react';
 const Home = () => (
     <div className="home-page">
         <h1 className="home-title">Mos Bros Casino</h1>
-        <div className="video-background">
-            <div className="video-foreground">
-                <iframe src="https://www.youtube.com/embed/ehrSceHtzV0?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=ehrSceHtzV0&mute=1" title="chips" frameBorder="0" allowFullScreen></iframe>
-            </div>
-        </div>
     </div>
 
 )
